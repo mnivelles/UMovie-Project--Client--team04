421b62ca21f036278ba19b3cbc9160af218f39ca
Mathieu Nivelles <mathieu.nivelles@gmail.com>
2015-10-30 16:34:15 -0400

    màj.

--- app/src/common.js
+++ app/src/common.js
@@ -4,6 +4,7 @@ define(function (require) {
 
     return {
         UMOVIE_API_BASE_URL: 'https://umovie.herokuapp.com/unsecure/',
+        TMDB_API_BASE_URL: 'https://api.themoviedb.org/3/',
         ENTER_KEY: 13
     };
 
---
+++ app/src/component/top/topList.model.js
@@ -0,0 +1,24 @@
+define(function (require) {
+
+    'use strict';
+
+    var Backbone = require('backbone'),
+        _ = require('underscore');
+
+    var movieBaseUrl = '/movies/';
+
+    return Backbone.Model.extend({
+        parse : function(data) {
+            var img = _.where(data['im:image'], {attributes: {
+                height: "170"
+            }
+            })[0]['label'];
+            var movie = {
+                title: data['im:name']['label'],
+                img: img.replace("170x170", "200x200"),
+                url: movieBaseUrl + data['id']['attributes']['im:id']
+            };
+            return movie;
+        }
+    });
+});
