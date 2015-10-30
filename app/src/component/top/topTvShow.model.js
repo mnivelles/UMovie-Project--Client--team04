c3c0ddc261fac2228e446b8ec9d5ff748e96c1ea
Mathieu Nivelles <mathieu.nivelles@gmail.com>
2015-10-29 21:46:39 -0400

    id de TvShow correspond à celui de la collection.

--- app/src/component/top/topTvShow.model.js
+++ app/src/component/top/topTvShow.model.js
@@ -6,16 +6,19 @@ define(function (require) {
 
     var tvShowBaseUrl = '/tv-shows/';
 
+    var idRegExp = /id([0-9]*)/;
+
     return Backbone.Model.extend({
         parse : function(data) {
             var img = _.where(data['im:image'], {attributes: {
                 height: "100"
             }
             })[0]['label'];
+            var collectionId = data['im:collection']['link']['attributes']['href'].match(idRegExp)[1];
             var tvShow = {
                 title: data['im:collection']['im:name']['label'],
                 img: img,
-                url: tvShowBaseUrl + data['id']['attributes']['im:id']
+                url: tvShowBaseUrl + collectionId
             };
             return tvShow;
         }
