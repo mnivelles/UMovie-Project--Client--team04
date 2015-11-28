32c7d38b904b2274d9fc41c6073df95e2cf8f36d
Pierre-Marc Levasseur <pmleva@hotmail.com>
2015-11-28 15:53:13 -0500

    Login fonctionnel

--- app/src/component/login/login.view.js
+++ app/src/component/login/login.view.js
@@ -4,9 +4,16 @@ define(function (require) {
 
     var Backbone = require('backbone'),
         Nunjucks = require('nunjucks'),
-        template = 'login.nunj.html';
+        UserModel = require('user.model'),
+        template = 'login.nunj.html',
+        Common = require('/js/common.js');
 
     return Backbone.View.extend({
+
+        events: {
+            'click .loginButton': 'login',
+            'click .signupButton': 'redirectToSignupPage'
+        },
 
         render: function() {
             var html = Nunjucks.render(template, {});
@@ -14,6 +21,29 @@ define(function (require) {
             this.changePageTitleWith('Login');
 
             return this;
+        },
+
+        login: function() {
+            $('.invalidInfo-card').hide();
+            var credentials = {
+                'email' : $('#email').val(),
+                'password': $('#password').val()
+            }
+            $.ajax({
+                url: Common.UMOVIE_API_BASE_URL_SECURED + 'login',
+                type: 'POST',
+                data: JSON.stringify(credentials),
+                contentType: 'application/json'
+            }).done(function(data){
+                $.cookie(Common.LOGIN_TOKEN_COOKIE, data.token);
+                Backbone.history.navigate('', true);
+            }).fail(function(){
+                $('.invalidInfo-card').show();
+            });
+        },
+
+        redirectToSignupPage: function() {
+            console.log("Redirect to signup page.")
         }
     });
 });
