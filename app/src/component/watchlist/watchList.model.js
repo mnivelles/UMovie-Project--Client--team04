94d88365d75ec2b577fe4fa8035ad13ebd40e15e
Mathieu Nivelles <mathieu.nivelles@gmail.com>
2015-12-05 22:11:58 -0500

    Bogue réglé : Quand quelqu'un crée une watchList sans le champ «name» (sic ?), la page des watchList ne s'affichait pas.

--- app/src/component/watchlist/watchList.model.js
+++ app/src/component/watchlist/watchList.model.js
@@ -29,8 +29,16 @@ define(function (require) {
                 }
             });
 
-            var title = data.name.trim();
-            var title = title || '[Nanashi-SansNom]';
+            var title = '';
+
+            if (!data.name) {
+                title = '';
+            } else {
+                title = data.name.trim();
+            }
+            
+            title = title || '[Nanashi-SansNom]';
+            
             return {
                 id: data.id,
                 title: title,
